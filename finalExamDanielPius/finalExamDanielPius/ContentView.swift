74d3a707c9c930968098419063a5d3b3e87e31d1
Daniel <114254657+SBDJAY@users.noreply.github.com>
2024-12-05 11:58:31 -0500

    Add files via upload

---
+++ finalExamDanielPius/finalExamDanielPius/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  finalExamDanielPius
+//
+//  Created by Daniel Pius on 2024-12-05.
+//
+//Daniel Pius
+//991675608
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ finalExamDanielPius/finalExamDanielPius/finalExamDanielPiusApp.swift
@@ -0,0 +1,19 @@
+//
+//  finalExamDanielPiusApp.swift
+//  finalExamDanielPius
+//
+//  Created by Daniel Pius on 2024-12-05.
+//
+//Daniel Pius
+//991675608
+
+import SwiftUI
+
+@main
+struct finalExamDanielPiusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
